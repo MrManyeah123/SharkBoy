0cbee7a4d17a96885589fea0cf01f72944ca1f04
Shark Boy <shadowrun131@gmail.com>
2023-08-05 16:35:06 +0000

    Lava Girl

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if ! grep -q "eula=true" eula.txt; then
+    echo "Do you agree to the Mojang EULA available at https://account.mojang.com/documents/minecraft_eula ?"
+    read -N 1 -p "[y/n] " EULA
+    if [ "$EULA" = "y" ]; then
+        echo "eula=true" > eula.txt
+        echo
+    fi
+fi
+"jre/jdk8u312-b07-jre/bin/java" -javaagent:log4jfix/Log4jPatcher-1.0.0.jar -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6144M -Xms4096M -jar forge-1.16.5-36.2.39.jar nogui
